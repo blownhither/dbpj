f178e9296338650fc5ecf372455ae5b5ee7cbfa8
Ma Ziyin <mzy_martin@hotmail.com>
2016-05-09 19:48:47 +0800

    modify creat.sqle

--- create.sql
+++ create.sql
@@ -1,15 +1,15 @@
 create table if not exists category (
 	category_title 	varchar(255),
-    category_desc 	text,				#description
+    category_desc 	text,				
     category_id 	smallint auto_increment not null,
 	primary key(category_id)
 );
 
 create table if not exists item (
-	item_name 		varchar(255),			# notes: varchar stores up to 255B for safe Chinese usage, use text otherwise
+	item_name 		varchar(255),			
     category_id 	smallint ,
     item_id 		int auto_increment not null,
-									# maybe other attributes
+									
     primary key(item_id),
     constraint category_id_item foreign key (category_id) references category(category_id)
 );
@@ -18,7 +18,7 @@ create table if not exists seller (
     store_name 		varchar(255),
     store_addr 		text,
     store_city 		varchar(255),
-    owner_id 		int,					# this ought to be personal identity id
+    owner_id 		int,					
     store_id 		int auto_increment not null,
     primary key(store_id)
 );
@@ -26,8 +26,8 @@ create table if not exists seller (
 create table if not exists inventory (
     item_id 		int not null,
     inventory_price decimal(5,2),
-	inventory_desc 	text,			# description
-    main_picture_url	text,			# normally used as preview photo
+	inventory_desc 	text,			
+    main_picture_url	text,			
     inventory_id 	int auto_increment not null,
     last_update		timestamp,
 	primary key(inventory_id),
@@ -49,7 +49,7 @@ create table if not exists private_sheet (
     constraint customer_id_private foreign key (customer_id) references customer(customer_id)
 );
 
-create table if not exists package (	# multiple entry for one package
+create table if not exists package (	
     inventory_id	int not null,
     package_id 		int auto_increment not null,
     primary key(package_id, inventory_id),
@@ -65,14 +65,14 @@ create table if not exists single_order (
     discount_sub	decimal(5,2) default 0.00,	
     order_date		datetime,
     payment_date 	datetime,
-    payment_status	enum('unpaid','paid','shipping','arrived','received','returning','refunded'),		# check
+    payment_status	enum('unpaid','paid','shipping','arrived','received','returning','refunded'),		
     last_update 	timestamp,
-    deliver_id		int unsigned,					# need constraint
+    deliver_id		int unsigned,					
     comment_inventory	text,
     comment_seller		text,
     order_id		int auto_increment not null,
     primary key(order_id),
-    constraint inventory_id_sorder foreign key (inventory_id) references inventory(inventory_id),			# should not duplicate contr
+    constraint inventory_id_sorder foreign key (inventory_id) references inventory(inventory_id),			
     constraint customer_id_sorder foreign key (customer_id) references customer(customer_id)
     
 );
@@ -85,24 +85,13 @@ create table if not exists package_order (
     discount_sub	decimal(5,2) default 0.00,	
     order_date		datetime,
     payment_date 	datetime,
-    payment_status	enum('unpaid','paid','shipping','arrived','received','returning','refunded'),		# check
+    payment_status	enum('unpaid','paid','shipping','arrived','received','returning','refunded'),		
     last_update 	timestamp,
-    deliver_id		int unsigned,					# need constraint
+    deliver_id		int unsigned,					
     comment_package		text,
     comment_seller		text,
     order_id		int auto_increment not null,
     primary key(order_id),
-    constraint package_id_porder foreign key (package_id) references package(package_id),			# should not duplicate contr
+    constraint package_id_porder foreign key (package_id) references package(package_id),			
     constraint customer_id_porder foreign key (customer_id) references customer(customer_id)
 );
-
-### sample data ###
-
-
-load data infile 
-
-#show char set;						# available character sets
-#show variables; 					# make sure utf-8 is used at all the place
-
-#notes:
-#note that decimal in Workbench works as ()
