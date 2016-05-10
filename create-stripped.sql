df3a7e1f8f20831c638727dc2e3e8422b248de35
Ma Ziyin <mzy_martin@hotmail.com>
2016-05-10 22:20:19 +0800

    modified

--- create-stripped.sql
+++ create-stripped.sql
@@ -5,6 +5,7 @@ drop table if exists seller;
 drop table if exists user_info;
 drop table if exists item;
 drop table if exists category;
+drop table if exists detail;
 
 create table if not exists category (
 	category_title 	varchar(255),
@@ -37,9 +38,11 @@ create table if not exists inventory (
     picture_url		text,
 	inventory_quantity	int,
 	category_id 	int,	
+	seller_id		int,
     inventory_id 	serial not null,
 	primary key(inventory_id),
-    foreign key (category_id) references category(category_id)
+    foreign key (category_id) references category(category_id),
+	foreign key (seller_id) references seller(seller_id)
 );
 
 create table if not exists customer (
@@ -53,18 +56,24 @@ create table if not exists customer (
 
 create table if not exists single_order (
     customer_id		int not null,
-    inventory_id	int not null,
-    quantity		int,
-    order_date		datetime year to fraction,
-    payment_date 	datetime year to fraction,
-    payment_status	smallint,
-    last_update 	datetime year to fraction,
+	payment_status	smallint,
     deliver_id		varchar(255),					
-    comment_inventory	text,
-    comment_seller		text,
+	order_date		datetime year to fraction,
+    payment_date 	datetime year to fraction,
+    last_update 	datetime year to fraction,
+	seller_id		int,
     order_id		serial not null,
-    primary key(order_id),
-    foreign key (inventory_id) references inventory(inventory_id),			
-    foreign key (customer_id) references customer(customer_id)
-    
+    primary key (order_id),
+    foreign key (customer_id) references customer(customer_id),
+	foreign key (seller_id) references seller(seller_id)        
+);
+
+create table if not exists detail(
+	order_id 		int not null,
+	inventory_id	int not null,
+	quantity		int,
+	comment_inventory	text,
+    comment_seller		text,
+    foreign key (inventory_id) references inventory(inventory_id),	
+	foreign key (order_id) references single_order(order_id)
 );
