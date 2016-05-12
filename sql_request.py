e7fc7adb58a30aae8b682c3e5b9f5169ebd3d999
Ma Ziyin <mzy_martin@hotmail.com>
2016-05-12 10:05:35 +0800

    rename sql_query, adding sql_request to be used by web

---
+++ sql_request.py
@@ -0,0 +1,93 @@
+import informixdb
+import os
+import sql_statement
+
+
+class SqlRequest:
+    def __init__(self):
+        self.__Database = 'd_1460371357365469'
+        self.__Server = 'ifxserver1'
+        self.__Username = 'tqbodnho'
+        self.__Password = 'JSe2lR1cH6'
+        self.__sellerName = 'seller'
+        self.__inventoryName = 'inventory'
+        self.__customerName = 'customer'
+        self.__userTabName = 'user_info'
+        self.__orderName = 'single_order'
+        self.__currentName = 'current'
+        self.__detailName = 'detail'
+        self.stat = sql_statement.SqlStatement()
+        self.conn = None
+        self.conn = informixdb.connect(self.__Database + '@' + self.__Server, self.__Username, self.__Password)
+        if not self.conn:
+            raise Exception("Failed to connect via SQL to informixdb")
+        else:
+            print("connected to database " + self.__Database)
+
+    @staticmethod
+    def _is_none(att):
+        if att is None:
+            print('Unspecified Key Fields ')
+            return True
+
+    @staticmethod
+    def _is_dict(obj):
+        if not isinstance(obj,dict):
+            print("Unexpected parameter type other than dict")
+
+    def __del__(self):
+        self.conn.commit()
+        self.conn.close()
+
+    def _new_cursor(self):
+        if self.conn is None:
+            print("reconnect to SQL server ...")
+            self.conn = informixdb.connect(self.__Database + '@' + self.__Server, self.__Username, self.__Password)
+            if self.conn is None:
+                raise Exception("Failed to connect via SQL to informixdb")
+        c = self.conn.cursor()
+        if c is None:
+            raise Exception("Failed to get a cursor on SQL connect")
+
+    def _sql_execute(self, sql_str):
+        cur = self._new_cursor()
+        cur.execute(sql_str)
+        cur.close()
+
+    def _sql_count(self, sql_str):
+        cur = self._new_cursor()
+        cur.execute(sql_str)
+        ans = cur.rowcount()
+        cur.close()
+        return ans
+
+    def _sql_fetchall(self, sql_str):
+        cur = self._new_cursor()
+        cur.execute(sql_str)
+        a = cur.fetchall()
+        cur.close()
+        return a
+
+    def add_category(self, dic):
+        if self._is_none(dic) or not self._is_dict(dic):
+            return
+        dic.get('title',None)
+
+
+
+
+
+
+
+
+    def exist_user(self, id):
+        if self._is_none(id):
+            return
+        sql_str = 'select * from ' + self.__userTabName + 'where user_id = ' + id.__str__()
+        ans = self._sql_count(sql_str)
+        if ans > 1:
+            print("User ID replication found on " + id.__str__())
+        return ans == 1
+
+
+
