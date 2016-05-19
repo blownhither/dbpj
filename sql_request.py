bfeb009fe14a18fc87698b0eda8477aa47fafa6a
Ma Ziyin <mzy_martin@hotmail.com>
2016-05-19 16:49:39 +0800

    'minor'

--- sql_request.py
+++ sql_request.py
@@ -393,7 +393,7 @@ class SqlRequest:
             return False
         for dic in dic_list:
             if 'payment_status' in dic:
-                dic['payment_status'] = self.__payment_status_dict[dic['payment_status']]
+                dic['payment_status'] = self.__payment_status_dict[int(dic['payment_status'])]
         return dic_list
 
     def search_order_seller(self, seller_id):
